99878bdcd8e5d2bba1372e75cfafe6d922c76350
federico-liccione <liccione.federico@icloud.com>
2022-02-06 03:34:50 +0100

    WIP: test command function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { Telegraf } = require('telegraf');
 const express  = require('express');
+require('dotenv').config();
 const app = express();
-
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
-    let message = ` Please use the /fact command to receive a new fact`
-    ctx.reply(message)
+    let message = ` Please use the menu to see the available options`;
+    ctx.reply(message);
 })
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.command('ora', (ctx) => ctx.replyWithDice());
+
+// Launch bot
 bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
