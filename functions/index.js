18eb52c946373c05d5b4a97034cbf97bbc527216
federico-liccione <liccione.federico@icloud.com>
2022-02-06 16:25:50 +0100

    WIP: add netlify functions

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,17 @@ bot.start((ctx) => {
     let message = ` Please use the menu to see the available options`;
     ctx.reply(message);
 })
+
+exports.handler = async event => {
+    try {
+      await bot.handleUpdate(JSON.parse(event.body));
+      return { statusCode: 200, body: '' };
+    } catch (e) {
+      console.log(e)
+      return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
+    }
+}
+  
 bot.command('ora', async (ctx) => {
     let htmlTemplate = await getCurrentSchedule();
     ctx.replyWithHTML(htmlTemplate);
